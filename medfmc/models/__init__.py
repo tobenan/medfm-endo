a1ba90f224bfefde158d33aeb646ef699840e35b
tobenan <640307541@qq.com>
2023-08-21 14:37:40 +0800

    新文件：   README.md

---
+++ medfmc/models/__init__.py
@@ -0,0 +1,9 @@
+from .prompt_swin import PromptedSwinTransformer
+from .prompt_vit import PromptedViT
+from .prompt_eva import PromptedViTEVA02
+
+__all__ = [
+    'PromptedViT', 
+    'PromptedSwinTransformer',
+    'PromptedViTEVA02'
+]
